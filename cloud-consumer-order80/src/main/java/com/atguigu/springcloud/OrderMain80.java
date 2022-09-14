610dfa4fc25f6d0868e08ff1a7758273373ea95c
mjq <mjq@qq.com>
2022-09-14 22:41:33 +0800

    2022年9月14日22:41:14 eureka注册

--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/OrderMain80.java
@@ -1,11 +1,14 @@
 package com.atguigu.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 /**
  * @auther zzyy
  * @create 2019-12-27 12:09
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderMain80
 {
     public static void main(String[] args)
